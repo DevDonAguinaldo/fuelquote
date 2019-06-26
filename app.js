b0374dcd55c15329274bd467e4b697096ef32c99
Don Aguinaldo <donald.aguinaldo@gmail.com>
2019-06-26 15:32:47 -0500

    Update 0.0.4 - UI/UX Updates

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express               = require('express'),
       mongoose              = require('mongoose'),
       passport              = require('passport'),
       passportLocalMongoose = require('passport-local-mongoose'),
+      flash                 = require('connect-flash'),
+      cookieParser          = require('cookie-parser'),
       URL                   = process.env.MONGODB_URI || "mongodb://localhost/clientinfo",
       PORT                  = process.env.PORT || 3030;
 
@@ -24,10 +26,12 @@ app.use(express.static("public"));
 app.use(require("express-session")({
     secret: "This is a secret",
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser('secret'));
+app.use(flash());
 
 // ROUTES
 app.use('/', require('./routes/index'));
--- routes/clients.js
+++ routes/clients.js
@@ -2,11 +2,21 @@ const LocalStrategy = require('passport-local').Strategy,
       passport      = require('passport'),
       Client        = require('../models/client'), 
       express       = require('express'),
+      session       = require('express-session'),
+      flash         = require('express-flash'),
+      cookieParser  = require('cookie-parser'),
       router        = express.Router();
 
 passport.use(new LocalStrategy(Client.authenticate()));
 passport.serializeUser(Client.serializeUser());
 passport.deserializeUser(Client.deserializeUser());
+// router.use(cookieParser('secret'));
+// router.use(sessiont({
+//     secret: "<secret string here>",
+//     resave: false,
+//     saveUninitialize: true
+// }));
+// router.use(flash());
 
 // CLIENT REGISTRATION PAGE
 router.get("/register", (req, res) => {
@@ -20,6 +30,8 @@ router.post("/register", (req, res) => {
     Client.register(newClient, req.body.registerpassword, (err, client) => {
         if (err) {
             console.log(err);
+            req.flash('error', 'There is already a user with that username!');
+            res.locals.message = req.flash();
             return res.render('register');
         }
         passport.authenticate("local", (_err, client) => {
@@ -28,13 +40,16 @@ router.post("/register", (req, res) => {
                 res.render('register');
             }
         });
-        res.redirect('/');
+        req.flash('success', 'Successfully registered!');
+        res.locals.message = req.flash();
+        res.render('register');
     });
 });
 
 // HOME PAGE - REQUIRES LOGIN
 router.get("/home", (req, res) => {
-    res.render('home');
+    let client = req.user;
+    res.render('home', { client: client });
 });
 
 // LOGIN PAGE
